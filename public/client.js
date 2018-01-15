6c72f2fa8b457cee20478f7d8ad648789025a9a2
vaidotasp <vaidotasp@gmail.com>
2018-01-15 17:37:45 -0500

    issues with Global Google API function

--- public/client.js
+++ public/client.js
@@ -1,100 +1,91 @@
+//global vars, need to refactor
+//let map
+let geolat = 33;
+let geolong = -77;
 //Gather location from user's browser
-let getGeolocation = function(options) {
-  return new Promise(function(resolve, reject) {
-    navigator.geolocation.getCurrentPosition(resolve, reject, options)
-  })
-}
+function initGeoLoc() {
+  console.log('Init Geo Loc');
+  let getGeolocation = function(options) {
+    return new Promise(function(resolve, reject) {
+      navigator.geolocation.getCurrentPosition(resolve, reject, options);
+    });
+  };
+  let geo_options = {
+    enableHighAccuracy: true,
+    maximumAge: 30000,
+    timeout: 15000
+  };
 
-let geo_options = {
-  enableHighAccuracy: true,
-  maximumAge: 30000,
-  timeout: 15000
-}
+  getGeolocation(geo_options)
+    .then(position => {
+      console.log('handle location being available');
+      console.log({
+        lat: position.coords.latitude,
+        long: position.coords.longitude
+      });
+      geolat = position.coords.latitude;
+      geolong = position.coords.longitude;
 
-//commented out for debug options-it works
-getGeolocation(geo_options)
-  .then(position => {
-    console.log({
-      lat: position.coords.latitude,
-      long: position.coords.longitude
+      initGoogleMap(position.coords.latitude, position.coords.longitude);
+      //initialize G Map with coords
     })
-  })
-  .catch(err => {
-    //handle the location being unavailable
-    console.log('user denied promp or it did not work')
-    console.error(err.message)
-  })
+    .catch(err => {
+      //handle the location being unavailable
+      console.log('user denied promp or it did not work');
+      console.error(err.message);
+    });
+}
 
 //Set up all event handlers here
-document.getElementById('enter').addEventListener('click', searchZip)
+document.getElementById('go').addEventListener('click', initGeoLoc);
+document.getElementById('enter').addEventListener('click', searchZip);
 
-function searchZip() {
-  //perform zip validation first
-  let zip = document.getElementById('zip').value
-  if (zip.length !== 5 || !/[0-9]/.test(zip)) {
-    console.log('validation failed, returning')
-    return
-  }
-  console.log('seach zip function fired')
-  console.log(`zip value (string) is this: ${zip}`)
-  //Fire Google Maps API search
-  initAPI(zip)
+var map;
+
+function initGoogleMap(lat, long) {
+  console.log(`${lat}, ${long}`);
+
+  let jsCustomMap = document.createElement('script');
+  jsCustomMap.type = 'text/javascript';
+  jsCustomMap.src =
+    'https://maps.googleapis.com/maps/api/js?key=AIzaSyD0QDQIBb8duJ3tRNRn6eM5JfVntCG4bCw&libraries=places&callback=initMap';
+
+  document.getElementsByTagName('head')[0].appendChild(jsCustomMap);
 }
-
-function initAPI(zip) {
-  console.log(`init API function fired with value ${zip}`)
-}
-
-//map initialization section FIXME:prob needs refactoring, works for now
-let jsCustomMap = document.createElement('script')
-jsCustomMap.type = 'text/javascript'
-jsCustomMap.src =
-  'https://maps.googleapis.com/maps/api/js?key=AIzaSyD0QDQIBb8duJ3tRNRn6eM5JfVntCG4bCw&libraries=places&callback=initMap'
-
-document.getElementsByTagName('head')[0].appendChild(jsCustomMap)
-
-let map
-
-function initMap() {
-  let centerLocation = new google.maps.LatLng(38.9044, -77.0321)
-
+function initMap(geolat, geolong) {
+  console.log(`${geolat}, ${geolong}`);
+  //let centerLocation = new google.maps.LatLng(38.9044, -77.0321)
+  let centerLocation = new google.maps.LatLng(geolat, geolong);
   map = new google.maps.Map(document.getElementById('map'), {
     center: centerLocation,
     zoom: 15
-  })
+  });
 
-  let service = new google.maps.places.PlacesService(map)
+  let service = new google.maps.places.PlacesService(map);
   service.nearbySearch(
     {
       location: centerLocation,
       radius: 500,
-      type: ['cafe']
+      type: ['cafe'] //FIXME:Can I filter this even deeper?
     },
     processResults
-  )
+  );
 }
 
 function processResults(results, status, pagination) {
   if (status !== google.maps.places.PlacesServiceStatus.OK) {
-    return
+    return;
   } else {
-    createMarkers(results)
+    createMarkers(results);
     //pagination logic goes here, skipping for now
   }
 }
 
 function createMarkers(places) {
-  let bounds = new google.maps.LatLngBounds()
-  let placesList = document.getElementById('places')
+  let bounds = new google.maps.LatLngBounds();
+  let placesList = document.getElementById('places');
 
   for (let i = 0, place; (place = places[i]); i++) {
-    let image = {
-      url: place.icon,
-      size: new google.maps.Size(71, 71),
-      origin: new google.maps.Point(0, 0),
-      anchor: new google.maps.Point(17, 34),
-      scaledSize: new google.maps.Size(25, 25)
-    }
     //TODO: need a marker for user's location too, maybe custom with a tooltip
     let marker = new google.maps.Marker({
       map: map,
@@ -102,9 +93,23 @@ function createMarkers(places) {
       title: place.name,
       animation: google.maps.Animation.DROP,
       position: place.geometry.location
-    })
-    placesList.innerHTMl += '<li>' + place.name + '</li>'
-    bounds.extend(place.geometry.location)
+    });
+
+    bounds.extend(place.geometry.location);
   }
-  map.fitBounds(bounds)
+  map.fitBounds(bounds);
+}
+
+//zip searching workflow
+function searchZip() {
+  //perform zip validation first
+  let zip = document.getElementById('zip').value;
+  if (zip.length !== 5 || !/[0-9]/.test(zip)) {
+    console.log('validation failed, returning');
+    return;
+  }
+  console.log('seach zip function fired');
+  console.log(`zip value (string) is this: ${zip}`);
+  //Fire Google Maps API search
+  //TODO: Need to conver zip to long/lat using Googles Geocoding API
 }
